54efc24caf9637c5cd16b483610b5ed12d946436
Dhirodatto Biswas <83452888+vardhiro@users.noreply.github.com>
2022-05-02 11:07:52 +0530

    Add files via upload

---
+++ components/_footer.php
@@ -0,0 +1,12 @@
+<hr>
+<img src="images/mwl.png" alt="" height="70"> and
+<a href="https://www.php.net">
+<img src="images/php.png" alt="" height="50"></a> with
+<a href="https://www.sqlite.org">
+<img src="images/db.jpg" style="background-color:white;" alt="" height="50"></a><br>
+&nbsp;Code hosted on <img src="images/gh.jpg" alt="" height="50">
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+
+</html>
